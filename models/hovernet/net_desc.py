1673dc71ac5ab3d805485650da3a32fae1fe14dc
Fernando Cervantes Sanchez <iie.fercer@gmail.com>
2022-09-23 10:56:36 -0400

    Modified config.py to load configuration parameters from a .json file

--- models/hovernet/net_desc.py
+++ models/hovernet/net_desc.py
@@ -160,29 +160,6 @@ class HoVerNetComp(Net):
         assert mode == 'compressed_rec', \
                 'Unknown mode `%s` for HoVerNetComp %s. Only support `compressed_rec`.' % mode
 
-        # self.d0 = ProjectionBlock(in_channels=128, out_channels=256,
-        #                           kernel_size=1,
-        #                           stride=1,
-        #                           padding=0,
-        #                           bias=False)
-        # self.d1 = ProjectionBlock(in_channels=128, out_channels=512,
-        #                           kernel_size=1,
-        #                           stride=1,
-        #                           padding=0,
-        #                           bias=False)
-        # self.d2 = ProjectionBlock(in_channels=128,
-        #                           out_channels=1024,
-        #                           kernel_size=1,
-        #                           stride=1,
-        #                           padding=0,
-        #                           bias=False)
-        # self.d3 = ProjectionBlock(in_channels=128,
-        #                           out_channels=1024,
-        #                           kernel_size=1,
-        #                           stride=1,
-        #                           padding=0,
-        #                           bias=False)
-
         self.d0 = ResidualBlock(128, [1, 3, 1], [64, 64, 256], 3, stride=1)
         self.d1 = ResidualBlock(128, [1, 3, 1], [128, 128, 512], 4, stride=1)
         self.d2 = ResidualBlock(128, [1, 3, 1], [256, 256, 1024], 6, stride=1)
@@ -300,9 +277,47 @@ class HoVerNetComp(Net):
 
 
 ####
+class HoVerNetCompProj(HoVerNetComp):
+    """Initialise HoVer-Net for compressed images using a projection method to
+    increase the number of features from the reconstructed image."""
+
+    def __init__(self, input_ch=48, nr_types=None, freeze=False, mode='compressed_rec', rec_model=None):
+        super().__init__()
+
+        # Overwrite the downsampling blocks for projection-based blocks
+        self.d0 = ProjectionBlock(in_channels=128, out_channels=256,
+                                  kernel_size=1,
+                                  stride=1,
+                                  padding=0,
+                                  bias=False)
+        self.d1 = ProjectionBlock(in_channels=128, out_channels=512,
+                                  kernel_size=1,
+                                  stride=1,
+                                  padding=0,
+                                  bias=False)
+        self.d2 = ProjectionBlock(in_channels=128,
+                                  out_channels=1024,
+                                  kernel_size=1,
+                                  stride=1,
+                                  padding=0,
+                                  bias=False)
+        self.d3 = ProjectionBlock(in_channels=128,
+                                  out_channels=2048,
+                                  kernel_size=1,
+                                  stride=1,
+                                  padding=0,
+                                  bias=False)
+
+        # TODO: pytorch still require the channel eventhough its ignored
+        self.weights_init()
+
+
+####
 def create_model(mode=None, **kwargs):
     if mode not in ['original', 'fast', 'compressed_rec']:
         assert "Unknown Model Mode %s" % mode
     if mode == 'compressed_rec':
         return HoVerNetComp(mode=mode, **kwargs)
+    if mode == 'compressed_rec_proj':
+        return HoVerNetCompProj(mode=mode, **kwargs)
     return HoVerNet(mode=mode, **kwargs)
