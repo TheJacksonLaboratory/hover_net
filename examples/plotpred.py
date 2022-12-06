8c42b26c11e4c8f544af253b8137f14a1dde7e5e
Fernando Cervantes Sanchez <iie.fercer@gmail.com>
2022-12-06 13:44:18 -0500

    Added evaluation module for testing

--- examples/plotpred.py
+++ examples/plotpred.py
@@ -1,50 +1,133 @@
-import os
+import math
+import sys
 import zarr
 import json
 import numpy as np
-
-from matplotlib.collections import PatchCollection
-from matplotlib.patches import Polygon
 import matplotlib.pyplot as plt
 
+from skimage import draw
 
-def plotpred(pred):
-    patches = []
+
+def plotpred(pred, shape, color=None, dst_img=None):
+    if color is None:
+        color = tuple([np.rint(np.random.rand() * 255 / 4).astype(dtype=np.uint8)] * 3)
+
+    if not isinstance(color, np.ndarray):
+        color = np.array(color).reshape(1, 3)
+
+    if dst_img is None:
+        dst_img = np.zeros((*shape, 3), dtype=np.uint8)
+
     for k in pred['nuc'].keys():
         contour = np.array(pred['nuc'][k]['contour'])
-        patches.append(Polygon(contour, True))
+        rr, cc = draw.polygon(contour[:, 1], contour[:, 0], shape=shape)
 
-    return patches
+        dst_img[rr, cc, :] = dst_img[rr, cc, :] + color
 
-pred_scr = json.load(open(r"C:\Users\cervaf\Documents\Logging\hover_net\result\TCGA-F9-A97G-01Z-00-DX1_1.json", "r"))
-pred_ref = json.load(open(r"C:\Users\cervaf\Documents\Logging\hover_net\result\TCGA-F9-A97G-01Z-00-DX1_1_wsi.json", "r"))
-pred_cmp_prj = json.load(open(r"C:\Users\cervaf\Documents\Logging\hover_net\result\TCGA-F9-A97G-01Z-00-DX1_1_comp.json", "r"))
-pred_cmp_res = json.load(open(r"C:\Users\cervaf\Documents\Logging\hover_net\result\TCGA-F9-A97G-01Z-00-DX1_1_comp_res.json", "r"))
+    return dst_img
 
-z = zarr.open(r"C:\Users\cervaf\Documents\Logging\hover_net\zarr\TCGA-F9-A97G-01Z-00-DX1_1.zarr")
 
-patches_scr = plotpred(pred_scr)
-patches_ref = plotpred(pred_ref)
-patches_cmp_prj = plotpred(pred_cmp_prj)
-patches_cmp_res = plotpred(pred_cmp_res)
+def checkpred(pred, shape):
+    pred_keys = list(pred.keys())
+    n_preds = len(pred_keys)
 
-patches_scr = PatchCollection(patches_scr, alpha=0.25)
-patches_ref = PatchCollection(patches_ref, alpha=0.25)
-patches_cmp_prj = PatchCollection(patches_cmp_prj, alpha=0.25)
-patches_cmp_res = PatchCollection(patches_cmp_res, alpha=0.25)
+    overlapped_preds = []
+    for i in range(n_preds - 1):
+        pred_i = pred[pred_keys[i]]
+        pred_area_i = np.zeros(shape, dtype=np.uint8)
+        pred_i_contour = np.array(pred_i['contour'])
+        pred_area_i[draw.polygon(pred_i_contour[:, 1], pred_i_contour[:, 0], shape=shape)] = 1
 
-patches_scr.set_color([1, 0, 0])
-patches_ref.set_color([1, 1, 0])
-patches_cmp_prj.set_color([0, 1, 1])
-patches_cmp_res.set_color([1, 0, 1])
+        for j in range(i + 1, n_preds):
+            pred_j = pred[pred_keys[j]]
+            pred_area_j = np.zeros(shape, dtype=np.uint8)
+            pred_j_contour = np.array(pred_j['contour'])
+            pred_area_j[draw.polygon(pred_j_contour[:, 1], pred_j_contour[:, 0], shape=shape)] = 1
+            
+            if np.max(pred_area_i + pred_area_j) >= 2:
+                overlapped_preds.append((pred_keys[i], pred_keys[j]))
 
-fig, ax = plt.subplots()
+    return overlapped_preds
 
-ax.imshow(np.moveaxis(z['0/0'][0, :, 0], 0, -1))
 
-ax.add_collection(patches_scr)
-ax.add_collection(patches_ref)
-ax.add_collection(patches_cmp_prj)
-ax.add_collection(patches_cmp_res)
+if __name__ == "__main__":
+    log_dir = sys.argv[1]
+    img_dir = sys.argv[2]
+    img = sys.argv[3]
+    pred_src = json.load(open("%s/%s_zarr.json" % (log_dir, img), "r"))
+    pred_ref = json.load(open("%s/%s.json" % (log_dir, img), "r"))
 
-plt.show()
+    z = np.moveaxis(zarr.open("%s/%s.zarr" % (img_dir, img), "r")['0/0'][0, :, 0], 0, -1)
+
+    patches_src = plotpred(pred_src, (z.shape[0], z.shape[1]), color=(127, 127, 127), dst_img=z//2)
+    patches_ref = plotpred(pred_ref, (z.shape[0], z.shape[1]), color=(127, 0, 0), dst_img=z//2)
+
+    n_chunks_h = int(math.ceil(z.shape[0] / 328))
+    n_chunks_w = int(math.ceil(z.shape[1] / 328))
+
+    plt.imshow(patches_src - patches_ref)
+    plt.show()
+
+    fig, (ax1, ax2) = plt.subplots(1, 2)
+    ax1.set_title('Reference')
+    ax1.imshow(patches_src)
+    ax2.set_title('Dask')
+    ax2.imshow(patches_ref)
+
+    for i in range(1, n_chunks_h):
+        ax2.plot([0, z.shape[1]], [328 * i, 328 * i], 'b-')
+        ax2.plot([0, z.shape[1]], [328 * i + 64, 328 * i + 64], 'c:')
+        ax2.plot([0, z.shape[1]], [328 * i - 64, 328 * i - 64], 'c:')
+
+    for i in range(1, n_chunks_w):
+        ax2.plot([328 * i, 328 * i], [0, z.shape[0]], 'b-')
+        ax2.plot([328 * i + 64, 328 * i + 64], [0, z.shape[0]], 'c:')
+        ax2.plot([328 * i - 64, 328 * i - 64], [0, z.shape[0]], 'c:')
+
+    plt.savefig("%s/patches_comp_overlay.png" % log_dir)
+    plt.show()
+
+    patches_src = plotpred(pred_src, (z.shape[0], z.shape[1]), color=(127, 127, 127))
+    patches_ref = plotpred(pred_ref, (z.shape[0], z.shape[1]), color=(127, 0, 0))
+
+    plt.imshow(patches_src - patches_ref)
+
+    for i in range(1, n_chunks_h):
+        plt.plot([0, z.shape[1]], [328 * i, 328 * i], 'b-')
+        plt.plot([0, z.shape[1]], [328 * i + 64, 328 * i + 64], 'c:')
+        plt.plot([0, z.shape[1]], [328 * i - 64, 328 * i - 64], 'c:')
+
+    for i in range(1, n_chunks_w):
+        plt.plot([328 * i, 328 * i], [0, z.shape[0]], 'b-')
+        plt.plot([328 * i + 64, 328 * i + 64], [0, z.shape[0]], 'c:')
+        plt.plot([328 * i - 64, 328 * i - 64], [0, z.shape[0]], 'c:')
+
+    plt.savefig("%s/patches_diff.png" % log_dir)
+    plt.show()
+
+    plt.subplot(1, 2, 1)
+    plt.imshow(patches_src)
+    plt.subplot(1, 2, 2)
+    plt.imshow(patches_ref)
+
+    for i in range(1, n_chunks_h):
+        plt.plot([0, z.shape[1]], [328 * i, 328 * i], 'b-')
+        plt.plot([0, z.shape[1]], [328 * i + 64, 328 * i + 64], 'c:')
+        plt.plot([0, z.shape[1]], [328 * i - 64, 328 * i - 64], 'c:')
+
+    for i in range(1, n_chunks_w):
+        plt.plot([328 * i, 328 * i], [0, z.shape[0]], 'b-')
+        plt.plot([328 * i + 64, 328 * i + 64], [0, z.shape[0]], 'c:')
+        plt.plot([328 * i - 64, 328 * i - 64], [0, z.shape[0]], 'c:')
+
+    plt.savefig("%s/patches_comp.png" % log_dir)
+    plt.show()
+
+    overlapped_preds_src = checkpred(pred_src['nuc'], (z.shape[0], z.shape[1]))
+    print('Overlapped predictions zarr-based', len(overlapped_preds_src))
+
+    overlapped_preds_ref = checkpred(pred_ref['nuc'], (z.shape[0], z.shape[1]))
+    print('Overlapped predictions dask-based', len(overlapped_preds_ref))
+
+    for ovp in overlapped_preds_ref:
+        print(ovp)
